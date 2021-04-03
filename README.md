7b95bad54e9923b734d3767b1a09af74e5970862
Boris <boris@Boris-MacBookPro-16.local>
2021-04-03 00:27:51 -0700

    documentation

--- README.md
+++ README.md
@@ -77,3 +77,43 @@ the meta tag.
 ### encode_html_entities(html)
 
 Replaces unsafe HTML characters with their safer equivalents.
+
+
+## Date_Util
+
+Function that helps to present dates to end user
+
+The `current_date` parameter should be set to current time `new Date()`. It's
+left configurable for unit testing purposes.
+
+```
+const { Date_Util } = WebUtil;
+
+var date = new Date();
+date.setDate(date.getDate() - 1)
+
+console.log( Date_Util.time_ago( date, new Date() ) ) // "1 day ago"
+```
+
+### short_date(past_date, current_date)
+
+Format date and time of a date object as consisely as possible, loosing
+precision when the date is too distant relative to the current date.
+
+### time_ago(past_date, current_date)
+
+Like short_date but shows date in terms of how many days, months, years ago it was.
+
+### time_remaining_util(future_date, current_date)
+
+Opposite of `time_ago`. You can use this to display time remaining until expiration. 
+
+### date_str_to_epoch("YYYY/MM/DD")
+
+Takes in a string formatted as above. Gives time in Unix Epoch of a certain date.
+
+### date_to_epoch(date_obj)
+
+Converts JavaScript Date object to Unix epoch time.
+
+
---
+++ date_util.js
@@ -0,0 +1,157 @@
+'use strict';
+
+SUBROSA.Date_Util=(function(){
+
+  var month_name_en = [ 'Jan','Feb','March', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+
+  function short_date(past_date, reference_date){
+    if (!reference_date){
+      reference_date = new Date();
+    }
+
+    var seconds = Math.floor((reference_date-past_date)/1000);
+    if (seconds < 0){
+      return "";
+    }
+
+    var minutes = Math.floor(seconds/60);
+    var hours = Math.floor(minutes/60);
+    var days = Math.floor(hours/24);
+    var years = Math.floor(days/365);
+
+    hours = hours-(days*24);
+    minutes = minutes-(days*24*60)-(hours*60);
+    seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+    var str;
+
+    if (years != 0){
+      str = (1900+past_date.getYear()).toString();
+    } else if (days != 0){
+      str = month_name_en[past_date.getMonth()]+" "+past_date.getDate();
+    } else if (hours != 0 || minutes > 0){
+      var time_hours = past_date.getHours();
+      var am_pm = time_hours >= 12 ? "PM" : "AM";
+      str = (time_hours % 12)+":"+zero_pad(past_date.getMinutes(),2)+" "+am_pm;
+    } else if (seconds != 0){
+      str = seconds+"s ago"
+    } else {
+      str = "now";
+    }
+    return str;
+  }
+
+  function zero_pad(value, count){
+    var result = value.toString();
+    count -= result.length;
+    while (count-- > 0){
+      result = "0"+result;
+    }
+    return result;
+  }
+
+  function time_ago(past_date, reference_date){
+    if (!reference_date){
+      reference_date = new Date();
+    }
+
+    var seconds = Math.floor((reference_date-past_date)/1000);
+    if (seconds < 0){
+      return "";
+    }
+
+    var minutes = Math.floor(seconds/60);
+    var hours = Math.floor(minutes/60);
+    var days = Math.floor(hours/24);
+    var years = Math.floor(days/365);
+
+    hours = hours-(days*24);
+    minutes = minutes-(days*24*60)-(hours*60);
+    seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+    var str;
+    var count = 0;
+
+    if (years != 0){
+      str = years+" "+plural(years, "year")+" ago";
+    } else if (days != 0){
+      str = days+" "+plural(days, "day")+" ago";
+    } else if (hours != 0){
+      str = hours+" "+plural(hours, "hour")+" ago";
+    } else if (minutes != 0){
+      str = minutes+" "+plural(minutes, "minute")+" ago";
+    } else if (seconds != 0){
+      str = seconds+"s ago";
+    } else {
+      str = "now";
+    }
+    return str;
+  }
+
+
+  function time_remaining_until(future_date, reference_date){
+    if (!reference_date){
+      reference_date = new Date();
+    }
+
+    var seconds = Math.floor((future_date - reference_date)/1000);
+    if (seconds < 0){
+      return "";
+    }
+
+    var minutes = Math.floor(seconds/60);
+    console.log("minutes: ", minutes);
+    var hours = Math.floor(minutes/60);
+    var days = Math.floor(hours/24);
+
+    hours = hours-(days*24);
+    minutes = minutes-(days*24*60)-(hours*60);
+    seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+    var tokens = [];
+    var str = "";
+    var count = 0;
+
+    if (days != 0){
+      str = days+" "+plural(days, "day");
+      count++;
+    }
+
+    if (hours != 0){
+      str = hours+" "+plural(hours, "hour");
+      count++;
+    }
+
+    if (minutes != 0){
+      str += (count == 1 ? "," : "")+" "+minutes+" "+plural(minutes, "minute");
+      count++;
+    }
+
+    if (seconds != 0){
+      if (count == 1){
+        str += " and ";
+      } else if (count >= 2){
+        str += ", and ";
+      }
+      str += seconds+" "+plural(seconds, "second");
+      count++;
+    }
+    return str;
+  }
+
+  function date_str_to_epoch(date_str){ // input is like this: 2019/01/30
+    var date = new Date(date_str + " 00:00:00"); 
+    var epoch = Math.round(date.getTime()/1000.0);
+    return epoch;
+  }
+
+  function date_to_epoch(date){
+    var epoch = Math.round(date.getTime()/1000.0);
+    return epoch;
+  }
+
+  return SUBROSA.module_exports({public:{
+    short_date,
+    time_remaining_until,
+    time_ago,
+    date_str_to_epoch,
+    date_to_epoch,
+  }});
+})();
