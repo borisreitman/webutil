c8f60f8ffbb7fac1b67f4fbbf24569a9611a3eb3
Boris <boris@Boris-MacBookPro-16.local>
2021-04-02 23:31:44 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # webutil
 Web Development utilities
+
+Contains various groupings of functions to achieve common tasks in web development.
+
+## UrlUtil
+
+Allows to manipulate query parameters of a URL. Parameters are called *options*.
+
+```
+  <script src="web_util/url_util.js"></script>
+  <script>
+    const { URL_Util } = WebUtil;
+    let query = location.search.substr(1);
+    console.log(URL_Util.decode_url_options(query));
+  </script>
+```
+
+
+### decode_url_options(url_query) 
+
+Returns a dictionary
+
+```
+  var options = decode_url_options("a=b&c=d")
+  console.log(options.c); // d
+```
+
+### encode_url_options(dict) 
+
+Returns url query 
+
+```
+  var query = encode_url_options({a:'b', c:'d'})
+  console.log(query); // a=b&c=d
+```
+
+
---
+++ url_util.js
@@ -0,0 +1,73 @@
+'use strict';
+
+if (typeof(WebUtil)=="undefined"){
+  WebUtil = {};
+}
+
+WebUtil.URL_Util=(function(){
+
+  function stringify_url_options(options){
+    var list = []
+    for (var k in options){
+      var v = options[k];
+      if (Array.isArray(v)){
+        for (var i=0, l=v.length; i<l; i++){
+          list.push(k+"="+encodeURIComponent(v[i]));
+        }
+      } else {
+        list.push(k+"="+encodeURIComponent(v));
+      }
+    }
+    return list.join("&");
+  }
+
+  function is_url(word){
+    if (word.match(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function decode_url_options(options_str, decode_required){
+    var tokens = options_str.split("&");
+    var options = {};
+    for (var i=0, l=tokens.length; i<l; i++){
+      var token = tokens[i];
+      var pair = token.split("=");
+      var k = pair[0];
+      var v = pair[1]; // decodeURIComponent?
+      if (decode_required){
+        v = decodeURIComponent(v);
+      }
+      options[k] = v;
+    }
+    return options;
+  }
+
+  function get_url_option(k){
+    var p={};
+    location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(s,k,v){p[k]=decodeURIComponent(v)})
+    return k?p[k]:p;
+  }
+
+  function add_option_to_url(url, name, value){
+    var pos = url.indexOf("?");
+    if (pos==-1){
+      return url+"?"+name+"="+encodeURIComponent(value);
+    }
+    var base_url = url.substr(0, pos);
+    var options = url.substr(pos+1);
+    var parsed_options = decode_url_options(options, true);
+    parsed_options[name] = value;
+    return base_url+"?"+stringify_url_options(parsed_options);
+  }
+
+  return {
+    add_option_to_url,
+    is_url,
+    stringify_url_options,
+    decode_url_options,
+    get_url_option
+  };
+})();
